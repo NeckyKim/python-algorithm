7cecca38668a73bb446e099177af47bd6a7c0919
NeckyKim <kyeryoong@gmail.com>
2022-10-21 04:29:14 +0900

    studied through 8. (221020)

--- CH7. Array/8.Trapping-Rain-Water.py
+++ CH7. Array/8.Trapping-Rain-Water.py
@@ -7,13 +7,13 @@ from typing import List
 
 class Solution:
     
-    # 풀이1. 투 포인터를 최대로 이동
+    # 풀이 1. 투 포인터를 최대로 이동
     
-    def TrappingWaterWithTwoPointers(height: List[int]):
+    def TrappingWaterUsingTwoPointers(height: List[int]):
         if not height:
             return 0
     
-        # 물 높이 변수 초기화
+        # 쌓인 물 높이 변수 초기화
         volume = 0
     
         # 투 포인터 인덱스 정의
@@ -31,18 +31,50 @@ class Solution:
             
             # 오른쪽이 크면 왼쪽을 이동
             if left_max <= right_max:
+                # 최대 높이와 현재 높이의 차이만큼 물 높이를 더함
                 volume = volume + left_max - height[left]
                 left = left + 1
             
             # 왼쪽이 크면 오른쪽을 이동
             else:
+                # 최대 높이와 현재 높이의 차이만큼 물 높이를 더함
                 volume = volume + right_max - height[right]
                 right = right - 1
                 
-            print(left, right)
-                
         return volume
     
+    
+    
+    # 풀이 2. 스택 쌓기
+    
+    def TrappingWaterUsingStack(height: List[int]):
+        
+        stack = []
+        
+        # 쌓인 물 높이 변수 초기화
+        volume = 0
+        
+        for i in range(len(height)):
+            # 변곡점을 만나는 경우
+            while stack and height[i] > height[stack[-1]]:
+                # 스택에서 꺼냄
+                top = stack.pop()
+                
+                if not len(stack):
+                    break
+                
+                # 이전과의 차이만큼 물 높이 처리
+                distance = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]]) - height[top]
+                
+                volume = volume + distance * waters
 
+            stack.append(i)
+            
+        return volume
+    
+    
+    
 height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-print(Solution.TrappingWaterWithTwoPointers(height))
+print(Solution.TrappingWaterUsingTwoPointers(height))
+print(Solution.TrappingWaterUsingStack(height))
